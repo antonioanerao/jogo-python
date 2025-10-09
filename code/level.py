5579b89ed237a1756f94426838970521d16d8eec
Antonio Anerão <anerao.junior@gmail.com>
2025-10-09 04:47:10 -0500

    add: camera

--- code/level.py
+++ code/level.py
@@ -8,7 +8,7 @@ from debug import debug
 class Level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstables_sprites = pygame.sprite.Group()
 
         self.create_map()
@@ -25,6 +25,24 @@ class Level:
                     self.player = Player((x, y), [self.visible_sprites], self.obstables_sprites)
 
     def run(self):
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         debug(self.player.direction)
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+        print(self.half_height)
+
+    def custom_draw(self, player):
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
