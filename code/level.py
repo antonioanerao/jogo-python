3c4454e7aaf7ec127d2faca5eae263a9e63e68ff
Antonio Anerão <anerao.junior@gmail.com>
2025-10-10 05:07:31 -0500

    add: map as background

--- code/level.py
+++ code/level.py
@@ -14,17 +14,18 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
+        # for row_index, row in enumerate(WORLD_MAP):
+        #     for col_index, col in enumerate(row):
+        #         x = col_index * TILESIZE
+        #         y = row_index * TILESIZE
 
-                if col == 'x':
-                    Tile((x, y), [self.visible_sprites, self.obstables_sprites], 'x')
-                if col == 'z':
-                    Tile((x, y), [self.visible_sprites, self.obstables_sprites], 'z')
-                if col == 'p':
-                    self.player = Player((x, y), [self.visible_sprites], self.obstables_sprites)
+        #         if col == 'x':
+        #             Tile((x, y), [self.visible_sprites, self.obstables_sprites], 'x')
+        #         if col == 'z':
+        #             Tile((x, y), [self.visible_sprites, self.obstables_sprites], 'z')
+        #         if col == 'p':
+        #             self.player = Player((x, y), [self.visible_sprites], self.obstables_sprites)
+        self.player = Player((1920, 1310), [self.visible_sprites], self.obstables_sprites)
 
     def run(self):
         self.visible_sprites.custom_draw(self.player)
@@ -39,11 +40,18 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_width = self.display_surface.get_size()[0] // 2
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
-        print(self.half_height)
+
+        # Creating the floor
+        self.floor_surf = pygame.image.load('../graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft=(0, 0))
 
     def custom_draw(self, player):
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
